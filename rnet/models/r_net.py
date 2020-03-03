79b6f6eb7adf7f464394d067593c8208608b2e98
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2020-03-03 02:17:47 -0800

    Attempt to use char embeddings but getting 0 len samples

--- rnet/models/r_net.py
+++ rnet/models/r_net.py
@@ -51,15 +51,15 @@ class RNet(torch.nn.Module):
         self.enable_search = True
 
         # construct model
-        self.embedding = GloveEmbeddingNPY(word_vectors, hidden_size, drop_prob)
-        # self.char_embedding = Embedding(char_vectors, hidden_size, drop_prob)
+        self.embedding = Embedding(word_vectors, hidden_size, drop_prob)
+        self.char_embedding = Embedding(char_vectors, hidden_size, drop_prob)
 
-        # self.char_encoder = CharEncoder(mode=hidden_mode,
-        #                                 input_size=char_embedding_size,
-        #                                 hidden_size=hidden_size,
-        #                                 num_layers=encoder_char_layers,
-        #                                 bidirectional=encoder_bidirection,
-        #                                 dropout_p=dropout_p)
+        self.char_encoder = CharEncoder(mode=hidden_mode,
+                                        input_size=char_embedding_size,
+                                        hidden_size=hidden_size,
+                                        num_layers=encoder_char_layers,
+                                        bidirectional=encoder_bidirection,
+                                        dropout_p=dropout_p)
         encode_in_size = word_embedding_size + hidden_size * encoder_direction_num
 
         self.encoder = MyStackedRNN(mode=hidden_mode,
@@ -108,22 +108,22 @@ class RNet(torch.nn.Module):
         self.init_ptr_hidden = AttentionPooling(encode_out_size, hidden_size)
 
     def forward(self, context, question, context_char=None, question_char=None, context_f=None, question_f=None):
-        # assert context_char is not None and question_char is not None #TODO reinstate
+        assert context_char is not None and question_char is not None #TODO reinstate
 
         # word-level embedding: (seq_len, batch, embedding_size)
         context_vec, context_mask = self.embedding(context)
         question_vec, question_mask = self.embedding(question)
 
         # char-level embedding: (seq_len, batch, char_embedding_size)
-        # context_emb_char, context_char_mask = self.char_embedding(context_char)
-        # question_emb_char, question_char_mask = self.char_embedding(question_char)
+        context_emb_char, context_char_mask = self.char_embedding(context_char)
+        question_emb_char, question_char_mask = self.char_embedding(question_char)
 
-        # context_vec_char = self.char_encoder(context_emb_char, context_char_mask, context_mask)
-        # question_vec_char = self.char_encoder(question_emb_char, question_char_mask, question_mask)
+        context_vec_char = self.char_encoder(context_emb_char, context_char_mask, context_mask)
+        question_vec_char = self.char_encoder(question_emb_char, question_char_mask, question_mask)
 
         # mix embedding
-        # context_vec = torch.cat((context_vec, context_vec_char), dim=-1)
-        # question_vec = torch.cat((question_vec, question_vec_char), dim=-1)
+        context_vec = torch.cat((context_vec, context_vec_char), dim=-1)
+        question_vec = torch.cat((question_vec, question_vec_char), dim=-1)
 
         # encode: (seq_len, batch, hidden_size)
         context_encode, _ = self.encoder(context_vec, context_mask)
--- train.py
+++ train.py
@@ -112,7 +112,7 @@ def main(args):
                 optimizer.zero_grad()
 
                 # Forward
-                log_p1, log_p2 = model(cw_idxs, qw_idxs)
+                log_p1, log_p2 = model(cw_idxs, qw_idxs) if args.model == 'bidaf' else model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
                 y1, y2 = y1.to(device), y2.to(device)
                 loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
                 loss_val = loss.item()
